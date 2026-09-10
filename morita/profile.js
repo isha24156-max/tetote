8eda2a29d022a31442e27e00277d56d42037f080
isha24156-max <isha24156@ict.shimanet.ed.jp>
2026-09-10 14:06:48 +0900

    Create profile.js

---
+++ morita/profile.js
@@ -0,0 +1,28 @@
+const registerBtn = document.getElementById("registerBtn");
+
+const nameInput = document.getElementById("nameInput");
+const emailInput = document.getElementById("emailInput");
+
+const completeModal = document.getElementById("completeModal");
+
+
+registerBtn.addEventListener("click", () => {
+
+    const name = nameInput.value.trim();
+    const email = emailInput.value.trim();
+
+    // 名前かメールアドレスが空の場合
+    if (name === "" || email === "") {
+        alert("名前とメールアドレスを入力してください。");
+        return;
+    }
+
+    // 登録完了を表示
+    completeModal.classList.remove("hidden");
+
+    // 2秒後に登録完了を消す
+    setTimeout(() => {
+        completeModal.classList.add("hidden");
+    }, 2000);
+
+});
