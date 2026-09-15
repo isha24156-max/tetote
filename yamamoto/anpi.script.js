b2a0b3a0a7ef81c4c47353d55e24cd80b134dd05
isha24160-netizen <isha24160@ict.shimanet.ed.jp>
2026-09-15 15:14:14 +0900

    Update anpi.script.js

--- yamamoto/anpi.script.js
+++ yamamoto/anpi.script.js
@@ -17,7 +17,7 @@ import {
 
 
 const firebaseConfig = {
-    apiKey:  "AIzaSyA6stEZ00HAtMNEuV...",
+    apiKey:  "AIzaSyA6stEZ00HAtMNEvUzG47zIUArCFJgsfTA",
     authDomain: "tetote-f459b.firebaseapp.com",
     projectId: "tetote-f459b",
     storageBucket: "tetote-f459b.firebasestorage.app",
