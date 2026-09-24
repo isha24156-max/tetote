5dfd37d3a7de4cdae9061a56ff010951177b3844
isha24148-star <isha24148@ict.shimanet.ed.jp>
2026-09-24 13:51:44 +0900

    Update consent.js

--- morita/consent.js
+++ morita/consent.js
@@ -1,10 +1,20 @@
 const consentCheckbox = document.getElementById("consentCheckbox");
 const nextBtn = document.getElementById("nextBtn");
 
+// すでに同意している場合は、この画面を表示しない
+if (localStorage.getItem("consentAgreed") === "true") {
+    window.location.href = "../yamamoto/login.html";
+}
+
 consentCheckbox.addEventListener("change", () => {
     nextBtn.disabled = !consentCheckbox.checked;
 });
 
 nextBtn.addEventListener("click", () => {
+
+    // 同意したことを保存
+    localStorage.setItem("consentAgreed", "true");
+
+    // 次の画面へ
     window.location.href = "../yamamoto/login.html";
 });
